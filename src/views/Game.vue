add57ec330af38e66222a7e13cdc02b019f8484f
spencerishere <spencercockburn@outlook.com>
2025-09-06 22:26:59 -0400

    updated route-related files to have more appropiate names and file names

---
+++ src/views/Game.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="game-links">
+    <router-link to="/archive">ARCHIVE</router-link>
+    <p>DAY #000</p>
+    <router-link to="/">HELP</router-link>
+  </div>
+  <div id="progress">
+    <p><u>1</u></p>
+    <p>2</p>
+    <p>3</p>
+    <p>4</p>
+    <p>5</p>
+  </div>
+  <ClueCard v-for="(clue, index) in CLUES" :label="clue.label" :time="clue.time" :id="index + 1" />
+  <YouTubePlayer />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { CLUES } from '@/config'
+import ClueCard from '@/components/ClueCard.vue'
+import YouTubePlayer from '@/components/YouTubePlayer.vue'
+
+export default defineComponent({
+  components: { ClueCard, YouTubePlayer },
+  data() {
+    return {
+      CLUES,
+    }
+  },
+})
+</script>
+
+<style scoped>
+#progress {
+  width: 20rem;
+  display: flex;
+  margin: 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1rem;
+}
+
+#game-links {
+  width: 28rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+</style>
