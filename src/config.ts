ae1724fb8dc1abf43512083ecca76fda920a486c
spencerishere <spencercockburn@outlook.com>
2025-09-07 13:36:08 -0400

    last clue now is 3 seconds instead of 3.5

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CLUES = [
   { label: '0.5 seconds', time: 0.5 },
   { label: '1 second', time: 1 },
-  { label: 'Start of song', time: 3.5 },
+  { label: 'Start of song', time: 3 },
 ]
