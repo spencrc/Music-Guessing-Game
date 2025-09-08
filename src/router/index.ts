ff8939ecc1774ecd020e568643a1cc65d89b0651
spencerishere <spencercockburn@outlook.com>
2025-09-07 21:01:04 -0400

    added not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Game from '../views/Game.vue'
 import Archive from '../views/Archive.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'archive',
       component: Archive,
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notfound',
+      component: NotFound,
+    }
   ],
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>404 PAGE NOT FOUND</p>
+    <p>CLICK <router-link to="/">HERE</router-link> TO GO HOME</p>
+</template>
+
+<style scoped>
+
+a {
+    text-decoration: underline;
+}
+
+</style>
