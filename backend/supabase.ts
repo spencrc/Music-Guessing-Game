9916b0d5d7144f1403f4e344ee5c8b9b32da00c9
spencerishere <spencercockburn@outlook.com>
2025-09-08 00:01:52 -0400

    added supabase

---
+++ backend/supabase.ts
@@ -0,0 +1,4 @@
+import { createClient } from "@supabase/supabase-js"
+import { SUPABASE_URL, SUPABASE_SERVICE_ROLE } from "./config"
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE)
