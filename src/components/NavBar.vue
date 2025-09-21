05162bad1f0f2c9fa4c466b0992521a39bd9f9ed
spencrc <145073726+spencrc@users.noreply.github.com>
2025-09-20 23:48:51 -0400

    created NavBar and ProgressDisplay components from contents of Game.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="game-links">
+    <router-link to="/archive">ARCHIVE</router-link>
+    <p>DAY #{{ day }}</p>
+    <router-link to="/">HELP</router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { starting_date } from '../../config.json'
+
+const diffMs = new Date().getTime() - new Date(starting_date).getTime()
+
+const day = ref(String(Math.floor(diffMs / (1000 * 60 * 60 * 24))).padStart(3, '0'))
+</script>
+
+<style>
+#game-links {
+  width: 28rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+</style>
---
+++ src/components/ProgressDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="progress">
+    <p v-for="n in num_songs" :key="n">
+      <u v-if="n === gameStore.round + 1">{{ n }}</u>
+      <span v-else>{{ n }}</span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { num_songs } from '../../config.json'
+import { useGameStore } from '@/stores/game'
+
+const gameStore = useGameStore()
+</script>
+
+<style>
+#progress {
+  width: 20rem;
+  display: flex;
+  margin: 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
